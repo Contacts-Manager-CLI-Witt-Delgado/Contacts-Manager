15afb2a1c7248d2452c0066802df845b7b5348a0
JordanWitt <wittjordan94@gmail.com>
2022-11-30 10:04:54 -0600

    fixed some things

--- src/Contacts/ContactsApp.java
+++ src/Contacts/ContactsApp.java
@@ -36,30 +36,38 @@ public class ContactsApp {
         System.out.println("-----------(  *  )----------");
         System.out.println("------------( | )-----------");
         System.out.println("Welcome to the Jedi Archives");
+
+        List<String> contacts = new ArrayList<>();
+        contacts.add("Anakin Skywalker | 123-234-4566");
+        contacts.add("Obi Wan Kenobi | 123-234-4566");
+        contacts.add("Boba Fett | 123-234-4566");
+        contacts.add("Mace Windu | 123-234-4566");
+        contacts.add("Padme | 123-234-4566");
+
+        Path path = Paths.get("./src/Contacts/contacts.txt");
+        Files.write(path, contacts);
+
         do {
             System.out.println("-------------------------------");
             System.out.println("1. View contacts\n2. Add a new contact\n3. Search a contact by name\n4. Delete existing contact\n5. Exit");
             System.out.println("-------------------------------");
-            List<String> contacts = new ArrayList<>();
-            contacts.add("Anakin Skywalker | 123-234-4566");
-            contacts.add("Obi Wan Kenobi | 123-234-4566");
-            contacts.add("Boba Fett | 123-234-4566");
-            contacts.add("Mace Windu | 123-234-4566");
-            contacts.add("Padme | 123-234-4566");
 
-            Path path = Paths.get("./src/Contacts/contacts.txt");
-            Files.write(path, contacts);
             Scanner in = new Scanner(System.in);
             input = in.nextInt();
 
             if (input == 1) {
                 printString(path);
+
             } else if (input == 2) {
                 System.out.println("Add new contact");
                 String input2 = in.next();
                 contacts.add(input2);
                 Files.write(path, contacts);
-                System.out.println(contacts);
+                for (String contact : contacts) {
+                    if (contact.contains(input2)) {
+                        System.out.println("You added " + contact + " to Jedi Archives");
+                    }
+                }
 
             } else if (input == 3) {
                 System.out.println("Please enter name.");
@@ -67,21 +75,20 @@ public class ContactsApp {
                 for (String contact : contacts) {
                     if (contact.contains(input3)) {
                         System.out.println(contact);
-//                    } else {
-////                        System.out.println("Sorry contact not found");
                     }
                 }
-
-//            } else {
-//                System.out.println("Please enter full name and number of contact you wish to delete.");
-//                String input4 = in.nextLine();
-//                for (String contact : contacts) {
-//                    if (contact.contains(input4)) {
-//                        contacts.remove(input4);
-//                        Files.write(path, contacts);
-//                        System.out.println(contacts);
-//                    }
-//                }
+            } else if (input == 4){
+                System.out.println("Please enter full name and number of contact you wish to delete.");
+                String input4 = in.next();
+                for (String contact : contacts) {
+                    if (contact.contains(input4)) {
+                        contacts.remove(input4);
+                        Files.write(path, contacts);
+                        printString(path);
+                    }
+                }
+            }else {
+                System.out.println("Bye");
             }
         } while (input > 0 && input < 5);
     }
